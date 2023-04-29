19c378826f87fb2f484c1bde2ddc1c1dbc855bcd
Daniel <daniel@danielwjyoung.com>
2023-04-29 03:57:29 -0500

    completed mobile resp and added header

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,7 +2,53 @@ import React from "react";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatGPT3">
+      <div className="gpt3__whatGPT3-content">
+        <div className="gpt3__whatGPT3-content__top">
+          <h2>What is GPT3</h2>
+          <p>
+            We so opinion friends me message as delight. 
+            Whole front do of plate heard oh ought. 
+            His defective nor convinced residence own. 
+            Connection has put impossible own apartments boisterous. 
+            At jointure ladyship an insisted so humanity he. 
+            Friendly bachelor entrance to on by.
+          </p>
+        </div>
+        <div className="gpt3__whatGPT3-content__middle">
+          <h1>The possibilities are beyond your imagination</h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt3__whatGPT3-content__bottom">
+          <div className="gpt3__whatGPT3-content__bottom-left">
+            <h2>Chatbots</h2>
+            <p>
+              We so opinion friends me message as delight. 
+              Whole front do of plate heard oh ought. 
+            </p>
+          </div>
+          <div className="gpt3__whatGPT3-content__bottom-middle">
+            <h2>Knowledgebase</h2>
+            <p>
+              At jointure ladyship an insisted so humanity he. 
+              Friendly bachelor entrance to on by. 
+              As put impossible own apartments b
+            </p>
+          </div>
+          <div className="gpt3__whatGPT3-content__bottom-right">
+            <h2>Education</h2>
+            <p>
+              At jointure ladyship an insisted so humanity he. 
+              Friendly bachelor entrance to on by. 
+              As put impossible own apartments b
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  )
 };
 
 export default WhatGPT3;
